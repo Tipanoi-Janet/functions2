bd4d7609a717c3409eb34f34b9d777f61632e1f1
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-11 20:57:06 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    //strings
+    val y = "My name is"
+    val name = "susan"
+    println(y + " " + name)
+
+    val text = "Banana"
+    println(text[0])
+    println(text[1])
+    println(text[2])
+    println(text[3])
+    println(text[4])
+    println(text[5])
+
+    val x = "African investor"
+    println(x. to Uppercase ())
+    println(x. to Lowercase())
+
+    val message1 = "ORD756DHUR"
+    val message2 = "PRDG543DHUE"
+    if  (message1 ==message2)
+}
